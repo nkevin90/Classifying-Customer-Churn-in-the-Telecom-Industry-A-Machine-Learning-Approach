5c9b3aa246cb639ac68e4664a39299b12f1db8b6
Kelvin Njenga <njengakelvin@kabarak.ac.ke>
2023-07-04 11:01:06 +0300

    Getting started streamlit

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 
 # Reading the dataset
 data = pd.read_csv('bigml_59c28831336c6604c800002a.csv')
+st.title('Customer churn in the Telecom Industry')
+st.subheader('This model predicts how likely a customer is likely to churn in the telco industry')
+
+# Displaying the pie chart
 counts = data['churn'].value_counts()
 plt.figure(figsize =(12,8))
 plt.pie(counts, labels=['Not Churned', 'Churned'], autopct='%1.1f%%', startangle=90)
